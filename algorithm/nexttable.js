27546ef88fdfbd1c8e5a9520b7995123008f1472
SeckWei <seckwei@users.noreply.github.com>
2015-01-11 23:03:58 +0000

    Update nexttable.js

--- algorithm/nexttable.js
+++ algorithm/nexttable.js
@@ -13,10 +13,10 @@ function nexttable(input){
   
   for(var failPos = 2; failPos < A.length; failPos++){
     var flag = false;
-    addLine(">" + failPos);
+    //addLine(">" + failPos);
     
     for(var i = 1; i < failPos; i++){
-      addLine("-->" + i);
+      //addLine("-->" + i);
       var subA = A.slice(i, failPos);
       var subB = A.slice(0, subA.length);
       
