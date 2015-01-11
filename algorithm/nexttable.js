97b3272101cc069a95e5e63a6b5df60aca95e619
SeckWei <seckwei@users.noreply.github.com>
2015-01-11 21:07:18 +0000

    Update nexttable.js
    Trying new string

--- algorithm/nexttable.js
+++ algorithm/nexttable.js
@@ -1,5 +1,5 @@
 /** Main Algorithm **/
-var A = ["A","C","A","C","B"];  // Input array
+var A = ["1","1","0","0","0"];  // Input array
 var B = [-2,-2,-2,-2,-2];// Next table
 
 B[0] = -1; // First value always -1
@@ -7,10 +7,10 @@ B[1] = (A[1]==A[0])? -1 : 0;
 
 for(var failPos = 2; failPos < A.length; failPos++){
   var flag = false;
-  addLine(">" + failPos);
+  //addLine(">" + failPos);
   
   for(var i = 1; i < failPos; i++){
-    addLine("-->" + i);
+    //addLine("-->" + i);
     var subA = A.slice(i, failPos);
     var subB = A.slice(0, subA.length);
     
