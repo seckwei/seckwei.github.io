fff1ec4cb34acd568d006ed08409cf1ed701b2cf
SeckWei <seckwei@users.noreply.github.com>
2015-01-11 22:16:56 +0000

    Update nexttable.js

--- algorithm/nexttable.js
+++ algorithm/nexttable.js
@@ -1,53 +1,58 @@
 /** Main Algorithm **/
-var A = ["1","1","0","0","0"];  // Input array
-var B = [-2,-2,-2,-2,-2];// Next table
-
-B[0] = -1; // First value always -1
-B[1] = (A[1]==A[0])? -1 : 0;
-
-for(var failPos = 2; failPos < A.length; failPos++){
-  var flag = false;
-  addLine(">" + failPos);
+function nexttable(input){
   
-  for(var i = 1; i < failPos; i++){
-    addLine("-->" + i);
-    var subA = A.slice(i, failPos);
-    var subB = A.slice(0, subA.length);
+  var A = input.split("");  // Input array
+  document.getElementById("result").innerHTML = A;
+  
+  var B = new Array(A.length);// Next table
+  
+  B[0] = -1; // First value always -1
+  B[1] = (A[1]==A[0])? -1 : 0;
+  
+  for(var failPos = 2; failPos < A.length; failPos++){
+    var flag = false;
+    addLine(">" + failPos);
     
-    var same = compareStrArray(subA, subB);
-    
-    if(same){
-      addLine("same? = " + same);
-      if(A[failPos] !== A[subA.length]){
-        addLine("inner same? = " + (A[failPos] === A[subA.length]));
-        B[failPos] = subA.length;
-        flag = true;
-        break;
-      }
-      else if(subB.length === 1){
-        B[failPos] = 0;
-        flag = true;
-        break;
-      }
-      else {
-        continue;
-      }
-    }
-    else{
-      if(subB.length === 1 && subB[0] !== A[failPos]){
-        B[failPos] = 0;
-        flag = true;
-        break;
+    for(var i = 1; i < failPos; i++){
+      addLine("-->" + i);
+      var subA = A.slice(i, failPos);
+      var subB = A.slice(0, subA.length);
+      
+      var same = compareStrArray(subA, subB);
+      
+      if(same){
+        addLine("same? = " + same);
+        if(A[failPos] !== A[subA.length]){
+          addLine("inner same? = " + (A[failPos] === A[subA.length]));
+          B[failPos] = subA.length;
+          flag = true;
+          break;
+        }
+        else if(subB.length === 1){
+          B[failPos] = 0;
+          flag = true;
+          break;
+        }
+        else {
+          continue;
+        }
       }
       else{
-        continue;
+        if(subB.length === 1 && subB[0] !== A[failPos]){
+          B[failPos] = 0;
+          flag = true;
+          break;
+        }
+        else{
+          continue;
+        }
       }
     }
+    
+    if(!flag) B[failPos] = -1;
   }
-  
-  if(!flag) B[failPos] = -1;
+  displayArray(B);
 }
-displayArray(B);
 
 /** Helper Functions **/
 function compareStrArray(arrA, arrB){
