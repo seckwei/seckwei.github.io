3b60db916153f24800842cdefe24fcd460d8ee67
SWLim <seckwei93@gmail.com>
2015-01-14 18:31:45 +0000

    Updated the queuing of Flips
    Automated the queuing of each add_flip with two functions:
    1) director(order)
    - In charge of determining the direction of next queue
    2) actor(order, direction)
    - runs add_flip according to order and direction
    - increments the order and calls director for next queue

--- css-fun/js/tiling.js
+++ css-fun/js/tiling.js
@@ -4,28 +4,26 @@ function add_flip(num){
 	$("[data-order='"+ num +"']").css("display", "inline-block");
 }
 
-add_flip(1);
-$("[data-order='1'] > #flip-right").one("animationend webkitAnimationEnd", function (){		
-	add_flip(2);
-});
-$("[data-order='2'] > #flip-right").one("animationend webkitAnimationEnd", function (){		
-	add_flip(3);
-});
-$("[data-order='3'] > #flip-down").one("animationend webkitAnimationEnd", function (){		
-	add_flip(4);
-});
-$("[data-order='4'] > #flip-left").one("animationend webkitAnimationEnd", function (){		
-	add_flip(5);
-});
-$("[data-order='5'] > #flip-left").one("animationend webkitAnimationEnd", function (){		
-	add_flip(6);
-});
-$("[data-order='6'] > #flip-down").one("animationend webkitAnimationEnd", function (){		
-	add_flip(7);
-});
-$("[data-order='7'] > #flip-right").one("animationend webkitAnimationEnd", function (){		
-	add_flip(8);
-});
-$("[data-order='8'] > #flip-right").one("animationend webkitAnimationEnd", function (){		
-	add_flip(9);
-});
+director(1);
+function director(order){
+	if (order != 9){
+		var dir;
+		if (order % 3 == 0) 
+			dir = "down";
+		else if (order == 1 || order == 2 || order == 7 || order == 8)
+			dir = "right";
+		else
+			dir = "left";
+		
+		if(order == 1) add_flip(1);
+		actor(order, dir);
+	}	
+}
+
+function actor(order, dir){
+	$("[data-order='"+order+"'] > #flip-"+dir).one("animationend webkitAnimationEnd", function (){		
+		order++;
+		add_flip(order);
+		director(order);
+	});
+}
