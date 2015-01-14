84f4bb989095006f3cc6d49f4931aee3ac6a63e0
SWLim <seckwei93@gmail.com>
2015-01-14 22:53:02 +0000

    Update - made tiling a class
    tiling class function list:
    1. play - run / reset the animation
    2. add_flip - add tile
    3. director - queues adding tiles
    4. actor - runs add_flip according to director's call

--- css-fun/js/tiling.js
+++ css-fun/js/tiling.js
@@ -1,29 +1,47 @@
 ﻿//alert("Hello");
 
-function add_flip(num){
-	$("[data-order='"+ num +"']").css("display", "inline-block");
-}
-
-director(1);
-function director(order){
-	if (order != 9){
-		var dir;
-		if (order % 3 == 0) 
-			dir = "down";
-		else if (order == 1 || order == 2 || order == 7 || order == 8)
-			dir = "right";
-		else
-			dir = "left";
+var tiling = {
+	
+	play: function(text){
 		
-		if(order == 1) add_flip(1);
-		actor(order, dir);
-	}	
-}
-
-function actor(order, dir){
-	$("[data-order='"+order+"'] > #flip-"+dir).one("animationend webkitAnimationEnd", function (){		
-		order++;
-		add_flip(order);
-		director(order);
-	});
+		if(text == "Play"){
+			$(".cw2 > .cw-title > .play").html("Reset");
+			tiling.director(1);
+		}
+		
+		if(text == "Reset"){
+			$(".cw2 > .cw-title > .play").html("Play");
+			$(".sq2#static").each(function(){
+				$(this).css("display", "none");
+			});
+		}
+	},
+	
+	add_flip: function(num){
+		$("[data-order='"+ num +"']").css("display", "inline-block");
+	},
+	
+	director: function(order){
+		console.log(order);
+		if (order != 9){
+			var dir;
+			if (order % 3 == 0) 
+				dir = "down";
+			else if (order == 1 || order == 2 || order == 7 || order == 8)
+				dir = "right";
+			else
+				dir = "left";
+			
+			if(order == 1) tiling.add_flip(1);
+			tiling.actor(order, dir);
+		}	
+	},
+	
+	actor: function(order, dir){
+		$("[data-order='"+order+"'] > #flip-"+dir).one("animationend webkitAnimationEnd", function (){		
+			order++;
+			tiling.add_flip(order);
+			tiling.director(order);
+		});
+	}
 }
