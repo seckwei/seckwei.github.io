e241746fa4d584544fe78f66ee3818ad29319272
SWLim <seckwei93@gmail.com>
2015-05-10 13:27:06 +0100

    Added OrbitControl and a ground mesh

--- threejs/js/controls.js
+++ threejs/js/controls.js
@@ -1,8 +1,11 @@
+var gui
+
 window.onload = function(){
-	var gui = new dat.GUI();
+	gui = new dat.GUI();
 
-	gui.add(box, "Reset");
+	gui.add(box, "reset").name('Reset');
 
+	// DIMENSION
 	var f1 = gui.addFolder('Dimension');
 	f1.open();
 
@@ -11,22 +14,19 @@ window.onload = function(){
 	f1.add(box.scale, "z", 0, 3).step(0.01).listen();
 
 
+	// ROTATION
 	var f2 = gui.addFolder('Rotation');
 	f2.open();
 
-	var rotation = {
-		rotateX : 0,
-		rotateY : 0,
-		rotateZ : 0
-	};
+	f2.add(box.rotation, "x", -3.14, 3.14).listen();
+	f2.add(box.rotation, "y", -3.14, 3.14).listen();
+	f2.add(box.rotation, "z", -3.14, 3.14).listen();
 
-	f2.add(rotation, "rotateX", -3.14, 3.14).listen().onChange(function(angle){
-		box.rotation.x = angle;
-	});
-	f2.add(rotation, "rotateY", -3.14, 3.14).listen().onChange(function(angle){
-		box.rotation.y = angle;
-	});
-	f2.add(rotation, "rotateZ", -3.14, 3.14).listen().onChange(function(angle){
-		box.rotation.z = angle;
-	});
+	// LOCATION
+	var f3 = gui.addFolder('Location');
+	f3.open();
+
+	f3.add(box.position, "x", -50, 50).listen();
+	f3.add(box.position, "y", -50, 50).listen();
+	f3.add(box.position, "z", -50, 50).listen();
 };
--- threejs/js/testfield.js
+++ threejs/js/testfield.js
@@ -1,10 +1,10 @@
-var scene, camera, renderer;
+var scene, camera, controls, renderer;
 var box;
 
 var init = function(){
 	// SCENE
-	var WIDTH = 800 || window.innerWidth;
-	var HEIGHT = 600 || window.innerHeight;
+	var WIDTH = window.innerWidth;
+	var HEIGHT = window.innerHeight;
 
 	// DEFAULT CAMM ATTR
 	var VIEW_ANGLE = 45;
@@ -23,12 +23,19 @@ var init = function(){
 
 	// CAMERA
 	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+	
+	camera.position.x = 80;
+	camera.position.y = 80;
+	camera.position.z = 300;
+
+	camera.lookAt(new THREE.Vector3(0,0,0));
+	
+	// Orbit Control
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 	// SCENE
 	scene = new THREE.Scene();
 	scene.add(camera);
-
-	camera.position.z = 300;
 
 	// START Renderer
 	renderer.setSize(WIDTH, HEIGHT);
@@ -45,6 +52,25 @@ var others = function(){
 	pointLight.position.z = 100;
 	scene.add(pointLight);
 
+	// Ground
+	var groundMesh = new THREE.MeshBasicMaterial( { color: 0xC3A687, side: THREE.DoubleSide } );
+	var groundGeo = new THREE.Geometry();
+
+	groundGeo.vertices.push(
+		new THREE.Vector3(  50, 0,  50),
+		new THREE.Vector3(  50, 0, -50),
+		new THREE.Vector3( -50, 0,  50),
+		new THREE.Vector3( -50, 0, -50)
+	);
+
+	groundGeo.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	groundGeo.faces.push( new THREE.Face3( 2, 1, 3 ) );
+
+	var ground = new THREE.Mesh(groundGeo, groundMesh);
+	ground.position.y = -0.5;
+
+	scene.add(ground);
+
 
 	// Sphere
 	var sphereMat = new THREE.MeshLambertMaterial( { color: 0x0000FF } );
@@ -53,29 +79,32 @@ var others = function(){
 	//scene.add(sphere);
 
 
-	// Cube
+	// Box
 	var boxMat = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
 	var boxGeo = new THREE.BoxGeometry( 30, 30, 30 );
 	box = new THREE.Mesh(boxGeo, boxMat);
 	
-	box.rotation.x = 0.3;
-	box.rotation.y = -0.3;
-
-	box.Reset = function(){
+	box.reset = function(){
 		box.scale.x = 1;
 		box.scale.y = 1;
 		box.scale.z = 1;
 
-		box.rotation.x = 0.3;
-		box.rotation.y = -0.3;
+		box.rotation.x = 0;
+		box.rotation.y = 0;
 		box.rotation.z = 0;
+
+		box.position.x = 30/2;
+		box.position.y = 30/2;
+		box.position.z = 30/2;
 	}
+	box.reset();
 
 	scene.add(box);
 };
 
 var animate = function(t){
 	renderer.render(scene, camera);
+	controls.update();
 	window.requestAnimationFrame(animate, renderer.domElement);
 };
 
